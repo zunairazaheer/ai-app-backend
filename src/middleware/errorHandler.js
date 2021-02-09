e8639cff852ad3ac8f939bda7975e3cec29b6e2e
Zunaira <zunaira.zaheer@edgec.io>
2021-02-09 12:51:28 +0500

    docs: add architecture diagram

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,4 +1,4 @@
-// File: src/auth/password-reset.js
+// File: src/middleware/errorHandler.js
 // Last modified: Tue Jan 20 12:19:17 AM PKT 2026
 // Change type: new
 
@@ -10,6 +10,3 @@ export function processData(data) {
 }
 
 export default { VERSION, processData };
-
-// Updated: 2021-02-09T12:51:28
-// Change: docs: add architecture diagram

9cdf85e6f49c1ca9a898d5fb140c5838e2e4ca50
Zunaira <zunaira.zaheer@edgec.io>
2021-02-19 17:24:33 +0500

    chore: update .gitignore file

--- src/auth/register.js
+++ src/auth/register.js
@@ -1,4 +1,4 @@
-// File: src/auth/password-reset.js
+// File: src/auth/register.js
 // Last modified: Tue Jan 20 12:19:17 AM PKT 2026
 // Change type: new
 
@@ -10,9 +10,3 @@ export function processData(data) {
 }
 
 export default { VERSION, processData };
-
-// Updated: 2021-02-09T12:51:28
-// Change: docs: add architecture diagram
-
-// Updated: 2021-02-19T17:24:33
-// Change: chore: update .gitignore file

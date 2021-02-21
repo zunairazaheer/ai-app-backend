7a43c27c0983401e91a425ce5ac90f98177064b4
Zunaira <zunaira.zaheer@edgec.io>
2021-02-21 09:08:22 +0500

    fix: memory leak in background worker

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -1,4 +1,4 @@
-// File: src/auth/register.js
+// File: tests/integration.test.js
 // Last modified: Tue Jan 20 12:19:17 AM PKT 2026
 // Change type: new
 
@@ -10,6 +10,3 @@ export function processData(data) {
 }
 
 export default { VERSION, processData };
-
-// Updated: 2021-02-21T09:08:22
-// Change: fix: memory leak in background worker

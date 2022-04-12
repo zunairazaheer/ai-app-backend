6dbf8a884dc5269bf536895513f584fdb1fa5da5
Zunaira <zunaira.zaheer@edgec.io>
2022-04-12 09:39:20 +0500

    chore: configure CI/CD pipeline

--- src/services/email.js
+++ src/services/email.js
@@ -46,3 +46,6 @@ export default { VERSION, processData };
 
 // Updated: 2022-03-10T15:39:44
 // Change: feat: add email verification
+
+// Updated: 2022-04-12T09:39:20
+// Change: chore: configure CI/CD pipeline

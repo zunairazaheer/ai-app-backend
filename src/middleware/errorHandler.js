61b71c27c8822ea231896adad4b682898f9eda9f
Zunaira <zunaira.zaheer@edgec.io>
2025-11-10 10:01:30 +0500

    refactor: consolidate database queries

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -196,3 +196,6 @@ export default { VERSION, processData };
 
 // Updated: 2025-10-19T13:44:18
 // Change: security: upgrade vulnerable dependencies
+
+// Updated: 2025-11-10T10:01:30
+// Change: refactor: consolidate database queries
